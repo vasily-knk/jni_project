486efb4e0415e0efb5d1564906a110ef80f4d89e
vasily.knk <vasily.knk@gmail.com>
2017-06-20 01:00:32 +0300

    generated files added

---
+++ src/org/vasya/Bar.java
@@ -0,0 +1,50 @@
+package org.vasya;
+
+public class Bar {
+    protected int i;
+    protected java.lang.String str;
+    protected java.lang.Float of;
+    protected java.lang.String ostr;
+    protected org.vasya.Baz obz;
+
+    public int getI() {
+        return this.i;
+    }
+
+    public void setI(int val) {
+        this.i = val;
+    }
+
+    public java.lang.String getStr() {
+        return this.str;
+    }
+
+    public void setStr(java.lang.String val) {
+        this.str = val;
+    }
+
+    public java.lang.Float getOf() {
+        return this.of;
+    }
+
+    public void setOf(java.lang.Float val) {
+        this.of = val;
+    }
+
+    public java.lang.String getOstr() {
+        return this.ostr;
+    }
+
+    public void setOstr(java.lang.String val) {
+        this.ostr = val;
+    }
+
+    public org.vasya.Baz getObz() {
+        return this.obz;
+    }
+
+    public void setObz(org.vasya.Baz val) {
+        this.obz = val;
+    }
+
+}
---
+++ src/org/vasya/Baz.java
@@ -0,0 +1,14 @@
+package org.vasya;
+
+public class Baz {
+    protected int hhh;
+
+    public int getHhh() {
+        return this.hhh;
+    }
+
+    public void setHhh(int val) {
+        this.hhh = val;
+    }
+
+}
